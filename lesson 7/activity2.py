069e51984fd43e2f4075731bef656199afe3c0c3
Rantoriu <bestobrothatodo7000@gmail.com>
2025-11-28 15:42:54 +0000

    Lesson 9 done

---
+++ lesson 7/activity2.py
@@ -0,0 +1,11 @@
+class student:
+    grade=10
+    name="Kelvin"
+    def introduction(self):
+        print("Hi i am a student")
+    def details(self):
+        print("My name is",self.name)
+        print("I study in Grade",self.grade)
+ob=student()
+ob.introduction()
+ob.details()
